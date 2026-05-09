be8bd883267de76e4552801e1512d69b09ffd43c
Ana Carvalho <ana.paoliello.de.lucena.carvalho@student.howest.be>
2026-05-09 18:24:53 +0200

    Survivor monitor its backpack/body
    This makes the survivor check its own health and inventory
    - this is done with a base class from Behaviour Tree Service

---
+++ Plugins/GameFeatures/CarvalhoAnaZombie/Source/CarvalhoAnaZombieRuntime/UBTService_UpdateStats.cpp
@@ -0,0 +1,57 @@
+﻿#include "UBTService_UpdateStats.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+#include "AIController.h"
+#include "Common/HealthComponent.h"
+#include "Common/InventoryComponent.h"
+#include "Items/Weapon.h"
+
+UBTService_UpdateStats::UBTService_UpdateStats()
+{
+	NodeName = "Update Survivor Stats";
+	bNotifyTick = true;
+}
+
+void UBTService_UpdateStats::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	// get Blackboard and AI Controller
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!BlackboardComp || !AIController) return;
+
+	// get survivor
+	APawn* SurvivorPawn = AIController->GetPawn();
+	if (!SurvivorPawn) return;
+
+	// monitor health
+	if (UHealthComponent* HealthComp = SurvivorPawn->FindComponentByClass<UHealthComponent>())
+	{
+		float CurrentHealth = HealthComp->GetHealth(); 
+        
+		// write to memory
+		BlackboardComp->SetValueAsFloat(FName("CurrentHealth"), CurrentHealth);
+	}
+
+	// monitor inventory
+	if (UInventoryComponent* InventoryComp = SurvivorPawn->FindComponentByClass<UInventoryComponent>())
+	{
+		bool bHasWeapon = false; // don't have a weapon by default
+
+		// get backpack items
+		TArray<ABaseItem*> Backpack = InventoryComp->GetInventory();
+
+		for (ABaseItem* Item : Backpack)
+		{
+			if (Item && Cast<AWeapon>(Item))
+			{
+				bHasWeapon = true;
+				break;
+			}
+		}
+
+		// write to memory
+		BlackboardComp->SetValueAsBool(FName("HasWeapon"), bHasWeapon);
+	}
+}
---
+++ Plugins/GameFeatures/CarvalhoAnaZombie/Source/CarvalhoAnaZombieRuntime/UBTService_UpdateStats.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "UBTService_UpdateStats.generated.h"
+
+UCLASS()
+class CARVALHOANAZOMBIERUNTIME_API UBTService_UpdateStats : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+	UBTService_UpdateStats();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
