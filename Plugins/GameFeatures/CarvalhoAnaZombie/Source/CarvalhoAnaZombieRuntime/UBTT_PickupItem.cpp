d887b0c9101d715da547aeb9bd08773982c576de
Ana Carvalho <ana.paoliello.de.lucena.carvalho@student.howest.be>
2026-05-09 18:50:57 +0200

    Updated the Behaviour tree to actually do all player behaviours!!
    Now the bt has Branch 1: FLEE, Branch 2: FIGHT, Branch 3: SCAVENGE, Branch 4: EXPLORE
    and since the player doesn move if i add things in front of the player the branches get ativated to different states!!
    
    Also created a UUBTT_PickupItem script
    to actually add items to the inventory right always to slot 0 but just so the states would work

---
+++ Plugins/GameFeatures/CarvalhoAnaZombie/Source/CarvalhoAnaZombieRuntime/UBTT_PickupItem.cpp
@@ -0,0 +1,44 @@
+﻿#include "UBTT_PickupItem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Common/InventoryComponent.h"
+#include "Items/BaseItem.h"
+
+UUBTT_PickupItem::UUBTT_PickupItem()
+{
+	NodeName = "Pickup Nearest Item";
+}
+
+EBTNodeResult::Type UUBTT_PickupItem::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// get brain and blackboard
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!AIController || !BlackboardComp) return EBTNodeResult::Failed;
+
+	// get survivor and inventory
+	APawn* SurvivorPawn = AIController->GetPawn();
+	if (!SurvivorPawn) return EBTNodeResult::Failed;
+
+	UInventoryComponent* InventoryComp = SurvivorPawn->FindComponentByClass<UInventoryComponent>();
+	if (!InventoryComp) return EBTNodeResult::Failed;
+
+	// get item
+	UObject* ItemObject = BlackboardComp->GetValueAsObject(FName("NearestItem"));
+	ABaseItem* ItemToPickup = Cast<ABaseItem>(ItemObject);
+
+	if (ItemToPickup)
+	{
+		// add to backpack - forcing to slot 0 for now (CHANGE THIS)
+		bool bSuccess = InventoryComp->GrabItem(0, ItemToPickup);
+
+		if (bSuccess)
+		{
+			// clear memory
+			BlackboardComp->ClearValue(FName("NearestItem"));
+			return EBTNodeResult::Succeeded;
+		}
+	}
+
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/CarvalhoAnaZombie/Source/CarvalhoAnaZombieRuntime/UBTT_PickupItem.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "UBTT_PickupItem.generated.h"
+
+UCLASS()
+class CARVALHOANAZOMBIERUNTIME_API UUBTT_PickupItem : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UUBTT_PickupItem();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
